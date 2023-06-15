c4c450e6974977c6e0deb2e3b537a295876ec5e3
nazimboudeffa <boudeffa@gmail.com>
2023-06-15 19:34:52 +0200

    Changed a word

--- db/setup.sh
+++ db/setup.sh
@@ -1,10 +1,10 @@
 echo "Creating DB..."
-mysql -e "create database neofreelance;"
+mysql -e "create database jobberbase;"
 echo "Creating Tables..."
 mysql neofreelance < /db/setup.sql
 echo "Creating dev user..."
 mysql -e "CREATE USER 'dev'@'localhost' IDENTIFIED WITH mysql_native_password BY 'dev123';"
-mysql -e "GRANT ALL PRIVILEGES ON neofreelance.* TO 'dev'@'localhost' WITH GRANT OPTION;"
+mysql -e "GRANT ALL PRIVILEGES ON jobberbase.* TO 'dev'@'localhost' WITH GRANT OPTION;"
 mysql -e "FLUSH PRIVILEGES;"
 echo "Enabling .htaccess overrides..."
 sed -i 's/<Directory \/>/<Directory \/var\/www\/html>/g' /etc/apache2/sites-available/000-default.conf
